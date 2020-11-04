46212591d4bdfc8118619f86f9152001ca30cf50
harsh8221 <harsh8221@gmail.com>
2020-11-04 09:24:58 +0530

    Comment and Image

--- scraper/articleScraper.py
+++ scraper/articleScraper.py
@@ -1,19 +1,19 @@
 import websiteLogin
 from bs4 import BeautifulSoup
 import requests
-
+import os
 
 imagesUrl = []
 
 # Author Name and Article Tag
 def authorAndTag(soup):
-    tags = soup.select(".post-meta a")
+    tags = soup.select(".content .post-meta a")
     Author = tags[0].text
     Tag = tags[1].text
 
 # title image
 def titleImage(soup,articleTitle):
-    tag = soup.select(".single-post-thumb img")
+    tag = soup.select(".content .single-post-thumb img")
     imageUrl = tag[0].get('src')
     if imageUrl == None:
         return
@@ -31,29 +31,71 @@ def titleImage(soup,articleTitle):
     
     open(imageName,'wb').write(image.content)
 
-    
-
 # Article Title
 def getTitleandImage(soup):
-    titleTag = soup.select("div.erm-title-wrapper")
+    titleTag = soup.select(".content div.erm-title-wrapper")
     articleTitle = titleTag[0].text
     titleImage(soup,articleTitle)
 
+def articleMedia(soup):
+    images = soup.select(".content .entry .attachment-thumbnail")
+    # print(images)
+    os.mkdir('Images')
+    i=0
+    for image in images:
+        i+=1
+        link = image.get('src')
+        if link == None:
+            continue
+        ima = None
+        for j in range(5):
+            try:
+                ima = requests.get(link)
+                break
+            except:
+                ima = None
+
+        if ima == None:
+            print(link)
+            continue
+
+        ## have to handle get error
+        imageName = "image-"+str(i) + ".jpeg"
+        open(os.path.join("Images",imageName),'wb').write(ima.content)
+
+        
 # Like and Dislike of article
 def articleLikeDislike(soup):
-    tags = soup.select(".entry .ldc-ul_cont span")
+    tags = soup.select(".content .entry .ldc-ul_cont span")
     Like = tags[0].text
     DisLike = tags[1].text
     print(Like)
     print(DisLike)
 
-def articleComments(soup):
-    tags = soup.select("")
+def articleComments(soup,):#query selector
+    tags = soup.select(".content .commentlist .comment")
+    for item in tags:
+        author = item.select(".author-comment cite.fn")
+        author = author[0].text
+        date = item.select(".author-comment .comment-meta a")
+        date = date[0].text
+        content = item.select(".comment-content p")
+        content = content[0].text
+        ldl = item.select(".comment-content .ldc-cmt-box span.ldc-ul_cont span")
+        like = ldl[0].text
+        dislike = ldl[1].text
+        print(author)
+        print(date)
+        print(content)
+        print(like)
+        print(dislike)
+        # print(item)
 
 
 # Article data
 def articleContent(soup):
-    contentTag = soup.select(".erm-content-wrapper")
+    contentTag = soup.select(".content .erm-content-wrapper")
+    articleMedia(soup)
     for item in contentTag:
         content = item.text
         print(content)
@@ -64,6 +106,9 @@ def articleExtractor(pageSource):
     soup = BeautifulSoup(pageSource,'html.parser')
     # articleLikeDislike(soup)
     getTitleandImage(soup)
+    # articleContent(soup)
+    # articleMedia(soup)
+    # articleComments(soup)
 
 
 
@@ -95,7 +140,7 @@ def getEdition(driver,Edition):
 
 
 username = "harsh8221"
-password = "Entelechy@123"
+password = ""
 driver = websiteLogin.Login(username,password)
 Edition = "http://entelechy.daiict.ac.in/public_html/entelechy_v2/?tag=edition93&category_name=&author="
 getEdition(driver,Edition)
